fbea10bf5fe22b8254f91caae64a38d05af788ea
MXBikeraGIT <mxbikeragit@gmail.com>
2026-04-29 09:07:00 +0800

    Update vulkan_wrapper.h

--- include/vulkan_wrapper.h
+++ include/vulkan_wrapper.h
@@ -7,26 +7,8 @@
 extern "C" {
 #endif
 
-VKAPI_ATTR VkResult VKAPI_CALL vkCreateInstance(const VkInstanceCreateInfo* pCreateInfo,
-                                                const VkAllocationCallbacks* pAllocator,
-                                                VkInstance* pInstance);
-VKAPI_ATTR void VKAPI_CALL vkDestroyInstance(VkInstance instance,
-                                             const VkAllocationCallbacks* pAllocator);
-VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vkGetInstanceProcAddr(VkInstance instance,
-                                                                const char* pName);
-VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vkGetDeviceProcAddr(VkDevice device,
-                                                              const char* pName);
-VKAPI_ATTR VkResult VKAPI_CALL vkEnumeratePhysicalDevices(VkInstance instance,
-                                                          uint32_t* pPhysicalDeviceCount,
-                                                          VkPhysicalDevice* pPhysicalDevices);
-VKAPI_ATTR void VKAPI_CALL vkGetPhysicalDeviceProperties(VkPhysicalDevice physicalDevice,
-                                                         VkPhysicalDeviceProperties* pProperties);
-VKAPI_ATTR void VKAPI_CALL vkGetPhysicalDeviceFeatures2(VkPhysicalDevice physicalDevice,
-                                                        VkPhysicalDeviceFeatures2* pFeatures);
-VKAPI_ATTR VkResult VKAPI_CALL vkCreateDevice(VkPhysicalDevice physicalDevice,
-                                              const VkDeviceCreateInfo* pCreateInfo,
-                                              const VkAllocationCallbacks* pAllocator,
-                                              VkDevice* pDevice);
+VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vk_icdGetInstanceProcAddr(VkInstance instance, const char* pName);
+VKAPI_ATTR VkResult VKAPI_CALL vk_icdNegotiateLoaderICDInterfaceVersion(uint32_t* pSupportedVersion);
 
 #ifdef __cplusplus
 }
